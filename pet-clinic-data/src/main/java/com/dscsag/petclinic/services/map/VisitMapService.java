060470c629beff068c36f1b2a6a0a061552f90d6
mli <maximilian.lincks@dscsag.com>
2022-03-16 11:35:37 +0100

    implement VisitMapService. closes #46

--- pet-clinic-data/src/main/java/com/dscsag/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/services/map/VisitMapService.java
@@ -1,7 +1,21 @@
 package com.dscsag.petclinic.services.map;
 
 import com.dscsag.petclinic.model.Visit;
+import com.dscsag.petclinic.services.OwnerService;
 import com.dscsag.petclinic.services.VisitService;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    private final OwnerService ownerService;
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit == null) throw new RuntimeException("Null Visit cannot be saved");
+        if(visit.getPet() == null) throw new RuntimeException("Visit with Null pet connot be saveed");
+        if(visit.getPet().getOwner() == null) throw new RuntimeException("Visits with Petzs with Null Owners cannot be saved");
+        ownerService.save(visit.getPet().getOwner());
+        return super.save(visit);
+    }
 }
