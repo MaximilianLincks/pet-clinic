500c5736c34f92e4de842f994f15403c0b1bc46f
mli <maximilian.lincks@dscsag.com>
2022-03-16 09:40:44 +0100

    implement BaseEntity. closes #35

--- pet-clinic-data/src/main/java/com/dscsag/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/model/BaseEntity.java
@@ -3,12 +3,19 @@ package com.dscsag.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+//tells jpa that we dont want an extra table
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-    //Long instead of long (Hibernate recommendation)
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
