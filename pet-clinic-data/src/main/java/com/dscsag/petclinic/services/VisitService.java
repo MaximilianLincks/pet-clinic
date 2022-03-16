9b827fe5a021813c8b29c37e3d041b3520e43551
mli <maximilian.lincks@dscsag.com>
2022-03-16 11:28:44 +0100

    implement VisitMapService. closes #46

---
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.dscsag.petclinic.services;
+
+import com.dscsag.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
--- pet-clinic-data/src/main/java/com/dscsag/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/services/map/VisitMapService.java
@@ -1,6 +1,7 @@
 package com.dscsag.petclinic.services.map;
 
 import com.dscsag.petclinic.model.Visit;
+import com.dscsag.petclinic.services.VisitService;
 
-public class VisitMapService extends AbstractMapService<Visit,Long>{
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
 }
