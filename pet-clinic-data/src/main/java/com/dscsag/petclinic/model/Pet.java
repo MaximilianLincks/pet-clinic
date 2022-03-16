ba9f02ff8fe83da8ad66ab61d9d7d27973cfa07a
mli <maximilian.lincks@dscsag.com>
2022-03-16 10:07:20 +0100

    implemented visit Entity. closes #38

--- pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,9 +22,15 @@ public class Pet extends BaseEntity{
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
+    //i dont think you should cascade here
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
     @Column(name = "name")
     private String name;
+
+
 }
--- pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Visit.java
@@ -3,13 +3,22 @@ package com.dscsag.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Table(name = "visits")
+@Entity
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
