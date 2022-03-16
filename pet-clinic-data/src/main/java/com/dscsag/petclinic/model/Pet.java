9e3dee4e93ebf44395f21a03b0911d8842b67d0c
mli <maximilian.lincks@dscsag.com>
2022-03-16 09:52:59 +0100

    implemented owner Entity. closes #36

--- pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/dscsag/petclinic/model/Pet.java
@@ -3,14 +3,26 @@ package com.dscsag.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @ManyToOne
+    @JoinColumn(name = "pet_type")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Column(name = "name")
     private String name;
 }
