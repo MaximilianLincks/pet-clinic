dacc555c773f9e34370def65d7a257e1a527c007
mli <maximilian.lincks@dscsag.com>
2022-03-21 17:54:52 +0100

    added create owner. closes #55

--- pet-clinic-web/src/main/java/com/dscsag/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/dscsag/controllers/OwnerController.java
@@ -5,13 +5,12 @@ import com.dscsag.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Locale;
 
@@ -66,6 +65,39 @@ public class OwnerController {
         }else {
             model.addAttribute("selections",results);
             return "owners/ownersList";
+        }
+    }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model){
+       model.addAttribute("owner",new Owner());
+       return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result){
+        if(result.hasErrors()){
+            return "owners/createOrUpdateForm";
+        }else{
+            ownerService.save(owner);
+            return String.format("redirect:/owners/%s",owner.getId());
+        }
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateOwnerForm(@PathVariable Long ownerId, Model model){
+        model.addAttribute("owner",ownerService.findById(ownerId));
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result, @PathVariable Long ownerId){
+        if(result.hasErrors()){
+            return "owners/createOrUpdateOwnerForm";
+        }else {
+            owner.setId(ownerId);
+            ownerService.save(owner);
+            return String.format("redirect:/owners/%s",owner.getId());
         }
     }
 }
